402e67ff838de99edda49bab8addb64184554df9
SieuDoTia <su_huynh@yahoo.com>
2016-10-31 06:31:26 +0700

    Tạo tệp

---
+++ Manjira/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Manjira
+//
+//  Created by 小小 on 20/11/2556.
+//  Copyright (c) 2556 BE 星凤. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
